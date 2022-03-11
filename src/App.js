b772cef79480772fd0403214ae2c6bc28a0b2989
jackson4-kayombo <jckayombo97@gmail.com>
2022-03-11 14:47:35 +0300

    clear headers

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Social from './Components/Social';
 function App() {
   return (
     <div className="App">
-      <h1>heading test</h1>
-      <h2>hello world</h2>
       <Social />
     </div>
   );
